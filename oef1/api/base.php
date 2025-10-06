ed2ded6f06ec30c50ef952d33a00467a6274386e
amine-hs09 <mohamedamine.hssinoui@student.odisee.be>
2025-10-06 17:03:05 +0200

    strucutur opdracht

--- oef1/api/base.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/inc/dbcon.php';
-require_once __DIR__ . '/inc/helpers.php';
-setCors();
-$method = $_SERVER['REQUEST_METHOD'];
-?>
--- oef1/webapp/script.js
+++
@@ -1,9 +0,0 @@
-const API = "https://TON-DOMAINE.be/oef1/api/";
-
-async function api(path, options={}){
-  const res = await fetch(API + path, {headers:{'Content-Type':'application/json'},...options});
-  return res.json();
-}
-(async()=>{
-  console.log("Ready to test API:", API);
-})();
