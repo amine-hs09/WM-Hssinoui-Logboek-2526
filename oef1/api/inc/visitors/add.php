d25af6524e48ff5271c232586f8f1597bf33b7ae
amine-hs09 <mohamedamine.hssinoui@student.odisee.be>
2025-10-08 17:15:14 +0200

    vervolg api

--- oef1/api/inc/visitors/add.php
+++ oef1/api/inc/visitors/add.php
@@ -1,22 +1,22 @@
 <?php
 // Nieuwe bezoeker toevoegen
-check_required_fields(["first_name","last_name","birth_date","email"]);
+check_required_fields(["first_name", "last_name", "birth_date", "email"]);
 
 $first = htmlentities($postvars['first_name']);
-$last  = htmlentities($postvars['last_name']);
+$last = htmlentities($postvars['last_name']);
 $birth = htmlentities($postvars['birth_date']);
 $email = htmlentities($postvars['email']);
 
 if (!$stmt = $conn->prepare("INSERT INTO visitors (first_name, last_name, birth_date, email) VALUES (?,?,?,?)")) {
-    die(json_encode(["error" => "Prepare failed", "mysqlError" => $conn->error, "status" => "fail"]));
+  die(json_encode(["error" => "Prepare failed", "mysqlError" => $conn->error, "status" => "fail"]));
 }
 
 if (!$stmt->bind_param("ssss", $first, $last, $birth, $email)) {
-    die(json_encode(["error" => "Bind failed", "mysqlError" => $conn->error, "status" => "fail"]));
+  die(json_encode(["error" => "Bind failed", "mysqlError" => $conn->error, "status" => "fail"]));
 }
 
 if (!$stmt->execute()) {
-    die(json_encode(["error" => "Execute failed", "mysqlError" => $stmt->error, "status" => "fail"]));
+  die(json_encode(["error" => "Execute failed", "mysqlError" => $stmt->error, "status" => "fail"]));
 }
 
 $id = $conn->insert_id;
